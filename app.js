3f3325a253c8529bafdfab3eea521e9444fa25f0
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2021-10-09 12:40:45 -0300

    versao atualizada

---
+++ app.js
@@ -0,0 +1,68 @@
+const app = require('./config/server');
+const db = require('./config/connectionBD')
+const port = process.env.PORT || 3000;
+const { jwt, jwtConfig } = require('./config/jwt');
+
+app.post('/cadastro/salvar-cadastro', async(req, res) => {
+    const { email, password } = req.body;
+    const values = [email, password];
+
+    const response = await db.query('SELECT * FROM usuario WHERE email = $1', [email])
+
+    if (response.rows.length === 0) {
+        await db.query('INSERT INTO usuario(email, senha) VALUES ($1, $2)', values);
+        res.status(201).send({ status: 'Usuário cadastrado com sucesso!' })
+    } else {
+        res.status(401).send({ status: 'Já existe um usuário com esse Email!' });
+    }
+});
+
+app.post('/login', async(req, res) => {
+
+    const user_email = req.body.email
+    const user_password = req.body.password
+    const values = [user_email];
+    const response = await db.query('SELECT * FROM usuario WHERE email = $1', values);
+
+    if (response.rows.length > 0) {
+        const { id_usuario, email, senha } = response.rows[0];
+
+        if (user_email === email && user_password === senha) {
+            const token = jwt.sign({ id_usuario: id_usuario }, jwtConfig.secret, { expiresIn: 600 });
+
+            return res.json({ auth: true, token });
+
+        } else { res.status(401).send({ status: 'A senha não corresponde ao usuário.' }); }
+
+    } else { res.status(401).send({ status: 'Email não encontrado.' }); }
+});
+
+app.post('/minhastarefas/salvar-tarefa', async(req, res) => {
+    const { nome, descricao } = req.body
+    const values = [nome, descricao]
+
+    await db.query('INSERT INTO tarefas (nome, descricao) VALUES ($1, $2)', values, (err, result) => {
+        res.redirect('/minhastarefas')
+    })
+
+})
+
+app.get('/minhastarefas/excluir', async(req, res) => {
+    var id = req.query.id
+
+    await db.query('DELETE FROM tarefas WHERE id_tarefa = $1', [id], (err, result) => {
+        res.redirect('/minhastarefas')
+    })
+})
+
+app.get('/cadastro', async(req, res) => { res.render('cadastro/cadastro'); });
+
+app.get('/minhastarefas', async(req, res) => {
+    const response = await db.query('SELECT * FROM tarefas')
+
+    res.render('lista/tarefas', { tarefas: response.rows });
+});
+
+app.get('/', async(req, res) => { res.render('login/login'); });
+
+app.listen(port, () => console.log(`Funcionando na porta ${port}`));
---
+++ config/jwt.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+const jwtConfig = {
+    secret: 'Hgvzmk6xOW6kXpdTs7auh$wPs7UK$lctsC3Sk@uiHSsTK$Zvs%',
+
+    verifyJWT(req, res, next) {
+        const token = req.headers['x-access-token'];
+        jwt.verify(token, this.secret, (err, decoded) => {
+            if (err) return res.status(401).send({ status: 'Token ausente ou inválido' });
+            req.id_usuario = decoded.id_usuario;
+            next();
+        })
+    },
+}
+
+module.exports = { jwt, jwtConfig }

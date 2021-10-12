43b3a159ea942a8499c5739ecd7be1964bfc5bff
Luis Eduardo <luiseduardo70@live.com>
2021-10-12 00:50:58 -0300

    adição e exclusão de tarefas na página de tarefas

--- app.js
+++ app.js
@@ -21,41 +21,30 @@ app.post('/cadastro/salvar-cadastro', async(req, res) => {
 });
 
 app.post('/login', async(req, res) => {
-    
     const user_email = req.body.email
     const user_password = req.body.password
     const values = [user_email];
+
     const response = await db.query('SELECT * FROM usuario WHERE email = $1', values);
-    
+
     if (response.rows.length > 0) {
         const { id_usuario, email, senha } = response.rows[0];
-        
         if (user_email === email && user_password === senha) {
             const token = jwt.sign({ id_usuario: id_usuario }, jwtConfig.secret, { expiresIn: 600 });
-            
             return res.json({ auth: true, token });
-            
         } else { res.status(401).send({ status: 'A senha não corresponde ao usuário.' }); }
-        
-    } else { res.status(401).send({ status: 'Email não encontrado.' }); }
-});
-
-app.get('/pergar-noticias:email', jwtConfig.verifyJWT, async (req, res) => {
-    const userEmail = req.body.email;
-    const response = await db.query('SELECT * FROM ');
+    }
+    else { res.status(401).send({ status: 'Email não encontrado.' }); }
 });
 
 app.post('/minhastarefas/salvar-tarefa', async (req, res) => {
-    console.log('entrou');
     const { userEmail, nome, descricao } = req.body;
     try{
         const response = await db.query('SELECT id_usuario FROM usuario where email = $1', [userEmail]);
         const userId = response.rows[0].id_usuario;
         const values = [nome, descricao, userId];
-        console.log(values);
-        
         await db.query('INSERT INTO tarefas (nome, descricao, fk_id_usuario) VALUES ($1, $2, $3)', values)
-        .then(response => {
+        .then(() => {
             res.redirect(`/minhastarefas/${userEmail}`);
         })
         .catch(e => {
@@ -69,24 +58,21 @@ app.post('/minhastarefas/salvar-tarefa', async (req, res) => {
 });
 
 app.get('/minhastarefas/excluir', async(req, res) => {
-    var id = req.query.id
-    
-    await db.query('DELETE FROM tarefas WHERE id_tarefa = $1', [id], (err, result) => {
-        res.redirect('/minhastarefas')
-    })
-})
+    const id = req.query.id;
+    const response = await db.query('SELECT * FROM usuario INNER JOIN tarefas ON id_usuario = fk_id_usuario WHERE id_tarefa = $1;', [id]);
+    const rows = response.rows[0];
+    const email = rows.email;
+    await db.query('DELETE FROM tarefas WHERE id_tarefa = $1', [id])
+    .then( () => res.redirect(`/minhastarefas/${email}`) );
+});
 
 app.get('/cadastro', async(req, res) => { res.render('cadastro/cadastro'); });
 
 app.get('/minhastarefas/:userEmail', async(req, res) => {
     const userEmail = req.params.userEmail;
-    
     const response = await db.query('SELECT id_usuario FROM usuario where email = $1', [userEmail]);
     const idUser = await response.rows[0].id_usuario;
-    
-    const response2 = await db.query('SELECT nome, descricao FROM tarefas WHERE fk_id_usuario = $1', [idUser]);
-    console.log('minhas tarefas:', response2.rows);
-    
+    const response2 = await db.query('SELECT id_tarefa, nome, descricao FROM tarefas WHERE fk_id_usuario = $1', [idUser]);
     res.render('lista/tarefas', { tarefas: response2.rows });
 });
 
